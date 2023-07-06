41139138bc8600e8f16694ebf0ddf2c64801985b
skmdab <57156958+skmdab@users.noreply.github.com>
2023-07-06 08:44:46 +0530

    Update aws_create.sh

--- aws_create.sh
+++ aws_create.sh
@@ -53,6 +53,6 @@ echo "$INSTANCENAME Server Created Successfully!"
 
 PUBLICIP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' | cut -d "[" -f2 | cut -d "]" -f1 | tr -d '" ')
 
-LINE="[$INSTANCENAME]\n\n$PUBLICIP ansible_user=ec2-user ansible_ssh_private_key_file=/home/ansible/filinta.pem"
+LINE="[$INSTANCENAME]\n\n$PUBLICIP ansible_user=ec2-user ansible_ssh_private_key_file=filinta.pem"
 
 echo "$LINE" > hosts
